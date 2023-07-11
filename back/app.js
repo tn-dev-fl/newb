03f935a64397d29d8a0b4c819d4fc8a7292a2d92
tn-dev-fl <rekik.mohamedali@iteam-univ.tn>
2023-07-12 00:19:25 +0100

    sorry

--- back/app.js
+++ back/app.js
@@ -24,7 +24,7 @@ app.post('/http', (req, res) => {
     })
     .catch(console.error);
 })
-app.get("/http_lines", async (req, res) => {
+app.get("/proxies_lines", async (req, res) => {
   try {
     console.log('here');
     const f1 = await countLines('public/files/http.txt');
@@ -39,6 +39,25 @@ app.get("/http_lines", async (req, res) => {
     console.log('error:', error);
   }
 });
+function getFirstFiveLines(filePath) {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createReadStream(filePath, { encoding: 'utf8' });
+    let lines = [];
+
+    stream.on('data', (data) => {
+      lines = data.split('\n').slice(0, 5);
+      stream.close();
+    });
+
+    stream.on('close', () => {
+      resolve(lines.map(JSON.parse));
+    });
+
+    stream.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
 
 
 function countLines(filePath) {
@@ -54,6 +73,27 @@ function countLines(filePath) {
   
   });
 }
+
+app.get('/json', (req, res) => {
+  const filePath = 'public/files/json.txt'; // Replace with your file path
+
+  getFirstFiveLines(filePath)
+    .then(lines => {
+      res.json({ proxies:lines });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+app.get('/date',(req,res)=>{
+  let ts = Date.now();
+  let date_time = new Date(ts);
+let date = date_time.getDate();
+let month = date_time.getMonth() + 1;
+let year = date_time.getFullYear();
+  res.json({date:year+'-'+month+'-'+date});
+})
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
